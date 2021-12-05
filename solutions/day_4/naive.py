50c2eb9dfd519a89ddb77b2c47640ea52f62b413
Mathijs <mathijsmul@gmail.com>
2021-12-05 14:51:51 +0100

    New day

--- solutions/day_4/naive.py
+++ solutions/day_4/naive.py
@@ -2,68 +2,62 @@ import fileinput
 import os
 
 import numpy as np
+from functools import reduce
 
 
 def read_file(input_path: str):
     input_path = os.path.join(
         os.path.abspath(os.path.dirname(__file__)), "..", "..", input_path
     )
+    content = fileinput.input(input_path)
 
-    boards = []
-    board = []
-    for idx, line in enumerate(fileinput.input(input_path)):
-        if idx == 0:
-            order = [int(nr) for nr in line.split(",")]
-        elif (idx - 1) % 6 == 0:
-            if len(board) > 0:
-                boards.append(board)
-            board = []
-        else:
-            board.append([int(x) for x in line.split()])
-    boards.append(board)
+    order = map(int, next(content).strip().split(","))
+    boards = np.array(
+        list(map(int, reduce(lambda x, y: x + y, content).split()))
+    ).reshape(-1, 5, 5)
 
-    for board in boards:
-        assert len(board) == 5
-        for row in board:
-            assert len(row) == 5
-
-    boards = [np.array(board) for board in boards]
     return order, boards
 
 
-def compute_score(board, bool_board, last_nr):
-    sum_remaining_nrs = np.sum(((1 - bool_board) * board).flatten())
-    return sum_remaining_nrs * last_nr
-
-
-def check_board(drawn_nrs, board):
-    bingo_bools = np.zeros((5, 5))
-    for nr in drawn_nrs:
-        bingo_locations = np.argwhere(board == nr)
-        for loc in bingo_locations:
-            bingo_bools[loc[0]][loc[1]] = 1
-
-        if any(x == 5 for x in bingo_bools.sum(0)) or any(
-            x == 5 for x in bingo_bools.sum(1)
-        ):
-            return compute_score(board, bingo_bools, nr)
+def compute_score(boards, bool_board, board_idx, last_nr):
+    return last_nr * np.sum(
+        (bool_board[board_idx] * boards[board_idx]).flatten()
+    )
 
 
 def solve(drawn_nrs, boards):
     winning_boards = []
+    hits = np.ones(boards.shape)
 
-    for idx, nr in enumerate(drawn_nrs):
-        for board_idx, board in enumerate(boards):
-            res = check_board(drawn_nrs[: idx + 1], board)
-            if res is not None and board_idx not in winning_boards:
-                winning_boards.append(board_idx)
-                print(f"Board {board_idx} wins with score {res}")
+    while len(winning_boards) < boards.shape[0]:
+        nr = next(drawn_nrs)
+        hits[boards == nr] = 0
+        winning_locs = np.argwhere(np.stack([hits.sum(dim) for dim in [1, 2]]) == 0)
+
+        if len(winning_locs) > 0:
+            winning_board_idx = winning_locs[0][1]
+            if winning_board_idx not in winning_boards:
+                res = compute_score(boards, hits, winning_board_idx, nr)
+                winning_boards.append(winning_board_idx)
+                print(f"Board {winning_board_idx} wins with score {res}")
 
 
 if __name__ == "__main__":
     sample_order, sample_boards = read_file("data/day_4/sample.txt")
     order, boards = read_file("data/day_4/input.txt")
+    # print(sample_order)
+    # print(sample_boards)
 
     # Part 1 & 2
     # solve(sample_order, sample_boards)
     solve(order, boards)
+
+    """
+    part 1
+    sample 4512
+    real 49686
+    
+    part 2 
+    sample 1924
+    real 26878
+    """
---
+++ solutions/day_5/naive.py
@@ -0,0 +1,100 @@
+import fileinput
+import os
+from typing import List, Tuple, Callable, Iterable
+import numpy as np
+import re
+
+
+def read_file(input_path: str):
+    input_path = os.path.join(
+        os.path.abspath(os.path.dirname(__file__)), "..", "..", input_path
+    )
+    # return [[list(map(int, coords.split(","))) for coords in line.strip().split("->")] for line in fileinput.input(input_path)]
+    return np.array(
+        [
+            list(map(int, re.split("\\->|,", line.strip())))
+            for line in fileinput.input(input_path)
+        ]
+    ).reshape(-1, 2, 2)
+
+
+def get_grid(lines):
+    # max_x = max(loc[0] for line in lines for loc in line)
+    # max_y = max(loc[1] for line in lines for loc in line)
+    max_x = max(lines[:, :, 0].flatten())
+    max_y = max(lines[:, :, 1].flatten())
+
+    return np.zeros((max_x + 1, max_y + 1))
+
+
+def get_score(grid):
+    return len(np.argwhere(grid >= 2))
+
+
+def fill_grid_straight(grid, input):
+    for vent in input:
+        loc1, loc2 = vent[0], vent[1]
+        # Select only straight lines
+        if loc1[0] == loc2[0]:
+            [start, end] = sorted([loc1, loc2], key=lambda coords: coords[1])
+            path = [[start[0], y] for y in range(start[1], end[1] + 1)]
+        elif loc1[1] == loc2[1]:
+            [start, end] = sorted([loc1, loc2], key=lambda coords: coords[0])
+            path = [[x, start[1]] for x in range(start[0], end[0] + 1)]
+        else:
+            continue
+
+        for loc in path:
+            grid[loc[0]][loc[1]] += 1
+
+    return get_score(grid)
+
+
+def fill_grid(grid, input):
+    for vent in input:
+        loc1, loc2 = vent[0], vent[1]
+
+        if loc1[0] == loc2[0]:
+            [start, end] = sorted([loc1, loc2], key=lambda coords: coords[1])
+            path = [[start[0], y] for y in range(start[1], end[1] + 1)]
+        elif loc1[1] == loc2[1]:
+            [start, end] = sorted([loc1, loc2], key=lambda coords: coords[0])
+            path = [[x, start[1]] for x in range(start[0], end[0] + 1)]
+        else:
+            xorder = sorted([loc1, loc2], key=lambda coords: coords[0])
+            xpath = list(range(xorder[0][0], xorder[1][0] + 1))
+
+            yorder = sorted([loc1, loc2], key=lambda coords: coords[1])
+            ypath = list(range(yorder[0][1], yorder[1][1] + 1))
+            if xorder[0].tolist() != yorder[0].tolist():
+                ypathrev = list(ypath)[::-1]
+                ypath = ypathrev
+
+            path = list(zip(xpath, ypath))
+        for loc in path:
+            grid[loc[0]][loc[1]] += 1
+
+    return get_score(grid)
+
+
+def solve_1(input_list):
+    grid = get_grid(input_list)
+    return fill_grid_straight(grid, input_list)
+
+
+def solve_2(input_list):
+    grid = get_grid(input_list)
+    return fill_grid(grid, input_list)
+
+
+if __name__ == "__main__":
+    sample_input = read_file("data/day_5/sample.txt")
+    real_input = read_file("data/day_5/input.txt")
+
+    # Part 1
+    assert solve_1(sample_input) == 5
+    assert solve_1(real_input) == 6283
+
+    # # Part 2
+    assert solve_2(sample_input) == 12, solve_2(sample_input)
+    assert solve_2(real_input) == 18864
