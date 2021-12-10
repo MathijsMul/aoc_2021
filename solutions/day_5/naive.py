7c24f0a88a48c6ecb3cd7c2128d1ca4446d856f1
Mathijs <mathijsmul@gmail.com>
2021-12-10 18:19:34 +0100

    Refactor day 5

--- solutions/day_5/naive.py
+++ solutions/day_5/naive.py
@@ -1,15 +1,14 @@
 import fileinput
 import os
-from typing import List, Tuple, Callable, Iterable
-import numpy as np
 import re
+
+import numpy as np
 
 
 def read_file(input_path: str):
     input_path = os.path.join(
         os.path.abspath(os.path.dirname(__file__)), "..", "..", input_path
     )
-    # return [[list(map(int, coords.split(","))) for coords in line.strip().split("->")] for line in fileinput.input(input_path)]
     return np.array(
         [
             list(map(int, re.split("\\->|,", line.strip())))
@@ -19,11 +18,8 @@ def read_file(input_path: str):
 
 
 def get_grid(lines):
-    # max_x = max(loc[0] for line in lines for loc in line)
-    # max_y = max(loc[1] for line in lines for loc in line)
     max_x = max(lines[:, :, 0].flatten())
     max_y = max(lines[:, :, 1].flatten())
-
     return np.zeros((max_x + 1, max_y + 1))
 
 
@@ -33,19 +29,17 @@ def get_score(grid):
 
 def fill_grid_straight(grid, input):
     for vent in input:
-        loc1, loc2 = vent[0], vent[1]
         # Select only straight lines
-        if loc1[0] == loc2[0]:
-            [start, end] = sorted([loc1, loc2], key=lambda coords: coords[1])
-            path = [[start[0], y] for y in range(start[1], end[1] + 1)]
-        elif loc1[1] == loc2[1]:
-            [start, end] = sorted([loc1, loc2], key=lambda coords: coords[0])
-            path = [[x, start[1]] for x in range(start[0], end[0] + 1)]
-        else:
-            continue
+        compared = np.argwhere(vent[0] - vent[1] == 0)
+        if len(compared) > 0:
+            axis = compared[0][0]
+            path1 = np.arange(min(vent[:, 1 - axis]), max(vent[:, 1 - axis]) + 1)
+            vent_size = path1.size
+            path2 = np.full(vent_size, vent[0, axis])
+            paths = [path1, path2]
 
-        for loc in path:
-            grid[loc[0]][loc[1]] += 1
+            for i in range(vent_size):
+                grid[paths[1 - axis][i], paths[axis][i]] += 1
 
     return get_score(grid)
 
