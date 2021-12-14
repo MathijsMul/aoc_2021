620c8a921584654425b6bcc2d86d669f9ae6d0c5
Mathijs <mathijs.mul@viqtordavis.com>
2021-12-14 08:49:56 +0100

    Clean

--- solutions/day_14/solution.py
+++ solutions/day_14/solution.py
@@ -1,6 +1,5 @@
 import os
-from collections import defaultdict, Counter
-import numpy as np
+from collections import Counter
 
 
 def read_file(input_path: str):
@@ -12,76 +11,46 @@ def read_file(input_path: str):
         if idx == 0:
             template = line.strip()
         elif "->" in line:
-            start, end = line.strip().split(" -> ")
-            rules[start] = end
+            pair, insertion = line.strip().split(" -> ")
+            rules[pair] = [pair[0] + insertion, insertion + pair[1]]
     return template, rules
 
 
-def solve_1_naive(input_template, insertion_rules, steps = 10):
-    for step in range(steps):
-        print(step)
-        polymer = ""
-        for idx in range(len(input_template) - 1):
-            pair = input_template[idx:idx + 2]
-            if idx == 0:
-                polymer += pair[0]
-            polymer += insertion_rules[pair] + pair[1]
-        input_template = polymer
-    counts = Counter(list(polymer)).most_common()
-    most_common_count = counts[0][1]
-    least_common_count = counts[-1][1]
-    return most_common_count - least_common_count
-
-
-def solve_1(template, insertion_rules, steps = 10):
-    extended_rules = {}
-    for pair in insertion_rules:
-        inserted = insertion_rules[pair]
-        extended_rules[pair] = [pair[0] + inserted, inserted + pair[1]]
-
-    stop = template[-1]
-    pair_count = Counter([template[i:i+2] for i in range(len(template) - 1)])
+def solve(template, insertion_rules, steps):
+    pair_count = Counter([template[i : i + 2] for i in range(len(template) - 1)])
 
     for step in range(steps):
-        # print(step)
         new_pair_count = Counter()
         for pair, count in pair_count.items():
-            out_pairs = extended_rules[pair]
-            for out_pair in out_pairs:
+            for out_pair in insertion_rules[pair]:
                 new_pair_count[out_pair] += count
 
         pair_count = new_pair_count
 
-    char_count = Counter([stop])
+    char_count = Counter([template[-1]])
     for pair, count in pair_count.items():
         char_count[pair[0]] += count
 
-    # chars = [pair[0] for pair in pairs] + [stop]
-    counts = char_count.most_common()
-    most_common_count = counts[0][1]
-    least_common_count = counts[-1][1]
-    return most_common_count - least_common_count
+    chars, counts = zip(*char_count.most_common())
+    return counts[0] - counts[-1]
 
 
+def solve_1(template, insertion_rules):
+    return solve(template, insertion_rules, 10)
 
-def solve_2(input_list):
-    return
+
+def solve_2(template, insertion_rules):
+    return solve(template, insertion_rules, 40)
 
 
 if __name__ == "__main__":
     sample_input = read_file("data/day_14/sample.txt")
     real_input = read_file("data/day_14/input.txt")
 
-    sample_template, sample_rules = sample_input
-
     # Part 1
     assert solve_1(*sample_input) == 1588, solve_1(*sample_input)
     assert solve_1(*real_input) == 2975
 
     # Part 2
-    sample_answer = solve_1(*sample_input, 40)
-    assert sample_answer == 2188189693529, sample_answer
-    # assert solve_2(sample2_input) == ..., solve_2(sample2_input)
-    # assert solve_2(sample3_input) == ..., solve_2(sample3_input)
-    print(solve_1(*real_input, 40))
-    # assert solve_2(real_input) == ...
+    assert solve_2(*sample_input) == 2188189693529
+    assert solve_2(*real_input) == 3015383850689
