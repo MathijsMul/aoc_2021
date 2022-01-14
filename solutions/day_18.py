31a43b17e007aeeddb66273e4806e81821c026c6
Mathijs <mathijs.mul@viqtordavis.com>
2022-01-14 10:08:07 +0100

    Remove many samples

--- solutions/day_18.py
+++ solutions/day_18.py
@@ -1,11 +1,8 @@
-import os
+from utils import read_file
 
 
-def read_file(input_path: str):
-    input_path = os.path.join(
-        os.path.abspath(os.path.dirname(__file__)), "..", "..", input_path
-    )
-    return [line.strip() for line in open(input_path).readlines()]
+def parse_input(input_path: str):
+    return [line.strip() for line in read_file(input_path)]
 
 
 def solve_1(input_list):
@@ -153,80 +150,15 @@ def get_max_magnitude(sfnrs):
 
 
 if __name__ == "__main__":
-    sample1_input = read_file("data/day_18/sample1.txt")
-    sample2_input = read_file("data/day_18/sample2.txt")
-    sample3_input = read_file("data/day_18/sample3.txt")
-    real_input = read_file("data/day_18/input.txt")
+    sample1_input = parse_input("data/day_18/sample1.txt")
+    sample2_input = parse_input("data/day_18/sample2.txt")
+    sample3_input = parse_input("data/day_18/sample3.txt")
+    real_input = parse_input("data/day_18/input.txt")
 
-    # Part 1
-    explode_sample_1 = "[[[[[9,8],1],2],3],4]"
-    explode_sample_2 = "[7,[6,[5,[4,[3,2]]]]]"
-    explode_sample_3 = "[[6,[5,[4,[3,2]]]],1]"
-    explode_sample_4 = "[[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]]"
-    explode_sample_5 = "[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]"
-    explode_sample_6 = "[[[[0,7],4],[[7,8],[0,[6,7]]]],[1,1]]"
-
-    assert explode(explode_sample_1) == "[[[[0,9],2],3],4]"
-    assert explode(explode_sample_2) == "[7,[6,[5,[7,0]]]]"
-    assert explode(explode_sample_3) == "[[6,[5,[7,0]]],3]"
-    assert explode(explode_sample_4) == "[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]"
-    assert explode(explode_sample_5) == "[[3,[2,[8,0]]],[9,[5,[7,0]]]]"
-    assert explode(explode_sample_6) == "[[[[0,7],4],[[7,8],[6,0]]],[8,1]]"
-
-    split_sample_1 = "[[[[0,7],4],[15,[0,13]]],[1,1]]"
-    split_sample_2 = "[[[[0,7],4],[[7,8],[0,13]]],[1,1]]"
-    assert split(split_sample_1) == "[[[[0,7],4],[[7,8],[0,13]]],[1,1]]"
-    assert split(split_sample_2) == "[[[[0,7],4],[[7,8],[0,[6,7]]]],[1,1]]"
-
-    reduce_sample_1 = "[[[[[4,3],4],4],[7,[[8,4],9]]],[1,1]]"
-    assert reduce(reduce_sample_1) == "[[[[0,7],4],[[7,8],[6,0]]],[8,1]]"
-
-    add_sample1 = "[[[[4,3],4],4],[7,[[8,4],9]]]"
-    add_sample2 = "[1,1]"
-    assert add_single(add_sample1, add_sample2) == "[[[[0,7],4],[[7,8],[6,0]]],[8,1]]"
-
-    # larger example
-    add_sample1 = "[[[0,[4,5]],[0,0]],[[[4,5],[2,6]],[9,5]]]"
-    add_sample2 = "[7,[[[3,7],[4,3]],[[6,3],[8,8]]]]"
-    assert (
-        add_single(add_sample1, add_sample2)
-        == "[[[[4,0],[5,4]],[[7,7],[6,0]]],[[8,[7,7]],[[7,9],[5,0]]]]"
-    )
-
-    add_sample1 = "[[[[4,0],[5,4]],[[7,7],[6,0]]],[[8,[7,7]],[[7,9],[5,0]]]]"
-    add_sample2 = "[[2,[[0,8],[3,4]]],[[[6,7],1],[7,[1,6]]]]"
-    assert (
-        add_single(add_sample1, add_sample2)
-        == "[[[[6,7],[6,7]],[[7,7],[0,7]]],[[[8,7],[7,7]],[[8,8],[8,0]]]]"
-    )
-
-    add_sample1 = "[[[[6,7],[6,7]],[[7,7],[0,7]]],[[[8,7],[7,7]],[[8,8],[8,0]]]]"
-    add_sample2 = "[[[[2,4],7],[6,[0,5]]],[[[6,8],[2,8]],[[2,1],[4,5]]]]"
-    assert (
-        add_single(add_sample1, add_sample2)
-        == "[[[[7,0],[7,7]],[[7,7],[7,8]]],[[[7,7],[8,8]],[[7,7],[8,7]]]]"
-    )
-
-    full_sample_1 = read_file("data/day_18/sample1.txt")
-    assert add(full_sample_1) == "[[[[5,0],[7,4]],[5,5]],[6,6]]"
-
-    full_sample_2 = read_file("data/day_18/sample2.txt")
-    assert add(full_sample_2) == "[[[[8,7],[7,7]],[[8,6],[7,7]]],[[[0,7],[6,6]],[8,7]]]"
-
-    assert compute_magnitude("[[1,2],[[3,4],5]]") == 143
-    assert compute_magnitude("[[[[0,7],4],[[7,8],[6,0]]],[8,1]]") == 1384
-    assert compute_magnitude("[[[[1,1],[2,2]],[3,3]],[4,4]]") == 445
-    assert compute_magnitude("[[[[3,0],[5,3]],[4,4]],[5,5]]") == 791
-    assert compute_magnitude("[[[[5,0],[7,4]],[5,5]],[6,6]]") == 1137
-    assert (
-        compute_magnitude("[[[[8,7],[7,7]],[[8,6],[7,7]]],[[[0,7],[6,6]],[8,7]]]")
-        == 3488
-    )
-
-    full_sample_3 = read_file("data/day_18/sample3.txt")
+    full_sample_3 = parse_input("data/day_18/sample3.txt")
     assert compute_magnitude(add(full_sample_3)) == 4140
 
-    real_input = read_file("data/day_18/input.txt")
+    real_input = parse_input("data/day_18/input.txt")
     assert compute_magnitude(add(real_input)) == 4137
 
     # Part 2
